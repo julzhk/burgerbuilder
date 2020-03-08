9f745728b9a3bab84c5c7b6b01ba8a553bc20b51
julian <julian.harley@gmail.com>
2020-03-08 12:32:25 +0000

    burger ingredients - empty at start

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,24 @@ import React from 'react';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
+    //get keys of ingredients state object
+    let transformedingredients = Object.keys(props.ingredients).map(
+        (igKey) => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient type={igKey} key={igKey + i}/>
+            })
+        }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    console.log(transformedingredients)
+    if (transformedingredients.length === 0) {
+        transformedingredients = <p>Please add something yummy!</p>
+    }
     return (
         <div className='Burger'>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
-            <BurgerIngredient type='salad'/>
-            <BurgerIngredient type='bacon'/>
+            {transformedingredients}
             <BurgerIngredient type='bread-bottom'/>
             Your Burger
         </div>
