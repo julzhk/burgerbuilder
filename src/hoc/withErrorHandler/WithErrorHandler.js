a156857655e44b2cbf8f54b737bb21d2622a9d80
julian <julian.harley@gmail.com>
2020-03-24 20:56:46 +0000

    add error handler in a modal, more -183

--- src/hoc/withErrorHandler/WithErrorHandler.js
+++ src/hoc/withErrorHandler/WithErrorHandler.js
@@ -11,21 +11,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
                 this.setState({error: null})
                 return req;
             });
-            axios.interceptors.response.use(res=>res, error => {
+            axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error})
-            })
+            });
         }
-        errorConfirmedHandler(){
-            this.setState({error:null})
-        }
+
+        errorConfirmedHandler() {
+            this.setState({error: null});
+        };
+
         render() {
             return (<Aux>
                     <Modal
                         show={this.state.error}
-                        clicked={this.errorConfirmedHandler}
+                        modalClosed={this.errorConfirmedHandler}
                     >
                         Oops - an error occurred! (
-                        {this.state.error ? this.state.error.message:null} )
+                        {this.state.error ? this.state.error.message : null} )
                     </Modal>
                     <WrappedComponent {...this.props} />
                 </Aux>
