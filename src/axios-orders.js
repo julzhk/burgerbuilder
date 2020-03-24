eec905ba8e0d0b25f73c91eae7da3ebc79956083
julian <julian.harley@gmail.com>
2020-03-24 20:47:14 +0000

    add error handler in a modal -183

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'https://<MYFIREBASE>.firebaseio.com/'
+    baseURL:'https://burgerbuilder-b8e8d.firebaseio.com/'
 });
 
 
--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../components/UI/Modal/Modal'
 import OderSummary from '../components/Burger/OrderSummary/OrderSummary'
 import axios from '../axios-orders'
 import Spinner from "../components/UI/Spinner/Spinner";
+import withErrorHandler from "../hoc/withErrorHandler/WithErrorHandler";
 
 const INGREDIENT_PRICES = {
     'salad': 3,
@@ -157,4 +158,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder
+export default withErrorHandler(BurgerBuilder,axios)
